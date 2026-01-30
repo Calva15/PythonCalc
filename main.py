330f3c05af5cd5cbc6cd127a59f0e1744977c863
Carlos Alvarez <alvarezcarlos232@gmail.com>
2026-01-30 13:05:12 -0600

    added comments for clarity

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 # Carlos Alvarez 1/30/2026
 #This is not a safe program to run because eval can be used to run code, but for something simple like this
 #It gets the job done.
-allowed_chars = "0123456789+-*/.()"
+allowed_chars = "0123456789+-*/.()" #Allowed characters for safety
 query = input("Enter a Math Expression: ").replace(" ","") #Input
 is_safe = all(char in allowed_chars for char in query) # Logic Check
 if is_safe and "**" not in query: #Extra check to prevent power operator
     try:
-        answer = eval(query) #Simply solves it, requires more for the program to be secure
+        answer = eval(query) #Safely evaluate the expression with some caveats
         print(answer) #Output
-    except (SyntaxError, ZeroDivisionError, OverflowError):
+    except (SyntaxError, ZeroDivisionError, OverflowError): #Catches common errors
         print("Error Invalid Input!")
 else:
-    print("Error Invalid Input!")
+    print("Error Invalid Input!") #If not safe, output error
